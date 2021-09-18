415eb38ecdba2338017fe27bd7e55462cdf39ffd
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-18 09:16:15 +0100

    Update mnist_pca.py
    added normalisation and plotting.

--- mnist_pca.py
+++ mnist_pca.py
@@ -2,11 +2,17 @@
 import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 data_csv = pd.read_csv(os.path.join(os.getcwd(), "mnist_train.csv"))
-data = np.array(data_csv)
+data_csv.drop("5", inplace=True, axis=1)
+data = np.array(data_csv, dtype=np.float64)
 
+#normalising data
+data/=255
+
+variances = np.zeros(data.shape[1]-2)
 #loop to check all possibilities from 1 single feature to #features-1
 for i in range(1, data.shape[1]-1):
     #if the PCA has a good explained variance ratio, we can reduce dimensions without losing much info
@@ -15,3 +21,10 @@ for i in range(1, data.shape[1]-1):
     pca = pca.fit(pca_input) 
     print("Explained Variance of "+str(i)+" adds up to {:.2f}%".format(100*np.sum(pca.explained_variance_ratio_)))
     #Z_pca = pca.transform(pca_input) <- commented as it wont be used
+    variances[i-1] = 100*np.sum(pca.explained_variance_ratio_)
+  
+plt.figure()
+plt.semilogx(variances)
+plt.xlabel("Features")
+plt.ylabel("Explained Variance in %")
+plt.grid()
