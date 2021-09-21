d31ea3065a90914154541b4f627c0190c976e790
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-21 18:12:50 +0100

    updated comments
    standardised comments across all repos

--- mnist_classifying.py
+++ mnist_classifying.py
@@ -14,35 +14,35 @@ x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
 x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
 
 # one-hot encoding
-y_train = to_categorical(y_train, num_classes=10)
-y_test = to_categorical(y_test, num_classes=10)
+y_train = to_categorical(y_train, num_classes = 10)
+y_test = to_categorical(y_test, num_classes = 10)
 
 # normalising pixels
-x_train = np.array(x_train, dtype='float64')
-x_test = np.array(x_test, dtype='float64')
+x_train = np.array(x_train, dtype = 'float64')
+x_test = np.array(x_test, dtype = 'float64')
 x_train /= 255
 x_test /= 255
 
 # I used this mode in a previous assignment in University
 model = Sequential()
-model.add(Conv2D(16, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(16, (3, 3), activation = 'relu', input_shape = (28, 28, 1)))
 model.add(MaxPooling2D())
-model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(Conv2D(32, (3, 3), activation = 'relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dense(10, activation='softmax'))
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.add(Dense(64, activation = 'relu'))
+model.add(Dense(10, activation = 'softmax'))
+model.compile(optimizer = 'Adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
 # fitting the model and then plotting it
-history = model.fit(x_train, y_train, batch_size=32, epochs=10, validation_split=0.1)
+history = model.fit(x_train, y_train, batch_size = 32, epochs = 10, validation_split = 0.1)
 plt.figure()
 plt.plot(history.history['loss'])
 plt.grid()
 
 # predictions
-y_pred = np.argmax(model.predict(x_test), axis=-1)
+y_pred = np.argmax(model.predict(x_test), axis = -1)
 
 # metrics calculated here
-cm = confusion_matrix(np.argmax(y_test, axis=1), y_pred)
-acc = 100*accuracy_score(y_test, to_categorical(y_pred, num_classes=10))
+cm = confusion_matrix(np.argmax(y_test, axis = 1), y_pred)
+acc = 100*accuracy_score(y_test, to_categorical(y_pred, num_classes = 10))
--- mnist_pca.py
+++ mnist_pca.py
@@ -6,21 +6,21 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 data_csv = pd.read_csv(os.path.join(os.getcwd(), "mnist_train.csv"))
-data_csv.drop("5", inplace=True, axis=1)
-data = np.array(data_csv, dtype=np.float64)
+data_csv.drop("5", inplace = True, axis = 1)
+data = np.array(data_csv, dtype = np.float64)
 
-#normalising data
+# normalising data
 data/=255
 
 variances = np.zeros(data.shape[1]-2)
-#loop to check all possibilities from 1 single feature to #features-1
+# loop to check all possibilities from 1 single feature to #features-1
 for i in range(1, data.shape[1]-1):
-    #if the PCA has a good explained variance ratio, we can reduce dimensions without losing much info
-    pca = PCA(copy=True, iterated_power='auto', n_components=i, random_state=None, svd_solver='auto', tol=0.0, whiten=False)
+    # if the PCA has a good explained variance ratio, we can reduce dimensions without losing much info
+    pca = PCA(copy = True, iterated_power = 'auto', n_components = i, random_state = None, svd_solver = 'auto', tol = 0.0, whiten = False)
     pca_input = data
     pca = pca.fit(pca_input) 
-    print("Explained Variance of "+str(i)+" adds up to {:.2f}%".format(100*np.sum(pca.explained_variance_ratio_)))
-    #Z_pca = pca.transform(pca_input) <- commented as it wont be used
+    print("Explained Variance of " + str(i) + " adds up to {:.2f}%".format(100*np.sum(pca.explained_variance_ratio_)))
+    # Z_pca = pca.transform(pca_input) <- commented as it wont be used
     variances[i-1] = 100*np.sum(pca.explained_variance_ratio_)
   
 plt.figure()
